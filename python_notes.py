aac63fba412b727b79d045de3d3b698f384bc763
Hankui Peng <569881571@qq.com>
2016-11-05 13:52:50 +0000

    10.05.2016

---
+++ python_notes.py
@@ -0,0 +1,22 @@
+print "Hello, world!"
+print(2+3)
+
+height = 1.67
+weight = 60
+bmi = weight/(height**2)
+
+# check out the type of a value
+type(bmi)
+
+day_of_week = 6
+type(day_of_week)
+
+x = "body mass index"
+type(x)
+
+z = True
+type(z)
+
+# sums of two integers, and sums of two strings
+2+3
+'ab'+'cd'
