1f71984a43e4a61f466ff76325767fef8cc361c0
Hankui Peng <569881571@qq.com>
2017-10-15 21:33:09 +0100

    10152017

--- python_notes.py
+++ python_notes.py
@@ -20,3 +20,16 @@ type(z)
 # sums of two integers, and sums of two strings
 2+3
 'ab'+'cd'
+
+# 12.02.2016
+# Create list baseball 
+baseball = [180, 215, 210, 210, 188, 176, 209, 200]
+
+# Import the numpy package as np
+import numpy as np
+
+# Create a Numpy array from baseball: np_baseball
+np_baseball = np.array(baseball)
+
+# Print out type of np_baseball
+print(type(np_baseball))
